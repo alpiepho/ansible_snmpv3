1e4f1456273c65d5a9243ea4e8f0a103b07bb3ac
apiepho <alpiepho@gmail.com>
2020-08-17 20:35:54 -0600

    finished TODOs

--- querytrace.sh
+++ querytrace.sh
@@ -21,16 +21,42 @@ FILES=/tmp/client_query_trace_*
 for f in $FILES
 do
   echo "Processing $f file..."
-  cat $f | cut -d " " -f 1,2,8,9,10 | sed -e "s/ /,/g" | sed -e "s/,/_/" -e "s/\?,/,/" -e "s/\[/__flags__/" -e "s/\]//" >> $resultname
-  # TODO: add host and client ip
-  # TODO: adjust for __flags__
+  # get host name from file name
+  host=$f
+  host=${host/\/tmp\/client_query_trace_//}
+  host=${host/\//}
+  host=${host/.txt/}
+   echo $host
+
+  if grep -Fxq "[" $f
+  then
+    # ie.: 
+    # 1          2               3  4                    5 6                   7  8     9  10
+    # |          |                                                                      |  |
+    # 2020-07-29 12:50:24.673603 IP 172.16.12.162.44580 > 172.16.12.52.53: 51581+ [1au] A? gamegogle.com. (54)
+    cat $f | cut -d " " -f 1,2,9,10 | sed -e "s/ /,/g" | sed -e "s/,/_/" -e "s/\?,/,/" | sed -e "s/,/,$host,$client_filter,/" >> $resultname
+  else
+    # ie.:
+    # 1          2               3  4                    5 6                   7    8  9
+    # |          |                                                                  |  |
+    # 2020-07-20 07:16:35.134816 IP 192.168.121.27.49077 > 192.168.121.101.53: 227+ A? microsoft. (27)
+    cat $f | cut -d " " -f 1,2,8,9 | sed -e "s/ /,/g" | sed -e "s/,/_/" -e "s/\?,/,/" | sed -e "s/,/,$host,$client_filter,/" >> $resultname
+  fi
 done
 
-# sort results file
+# remove blank lines and sort results file
+sed -i '/^$/d' $resultname
 sort -o $resultname $resultname
 
+# add header line 
+# Timestamp	DNS Server	Client IP Address	QType	Domain Name
+sed -i '1 i\Timestamp,DNS Server,Client IP Address,QType,Domain Name' $resultname
+
+# conver to tabs
+sed -i -e "s/,/	/g" $resultname
+
 # clean up trace files
-rm -f /tmp/client_trace_*.txt
+rm -f /tmp/client_query_*.txt
 
 echo "Results in $resultname"
 
